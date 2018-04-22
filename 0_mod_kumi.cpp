5402d80a2c7aa4764a9c59a180d8524a0d03b976
fuppy0716 <n.fuppy@gmail.com>
2018-04-22 19:40:35 +0900

    第二種スターリング数追加

--- 0_mod_kumi.cpp
+++ 0_mod_kumi.cpp
@@ -24,11 +24,16 @@ int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 };
 
 
 
-ll MAXM = 100100;
-vl fact(MAXM);
-vl rfact(MAXM);
+ll n, MOD;
+const int MAXN = 3010;
+vl ways(MAXN);
+vll ways2(MAXN, vl(MAXN));
 
-ll mod_pow(ll x, ll p, ll M) {
+vll secondStirling(MAXN, vl(MAXN, 0));
+vl fact(MAXN);
+vl rfact(MAXN);
+
+ll mod_pow(ll x, ll p, ll M = MOD) {
 	ll a = 1;
 	while (p) {
 		if (p % 2)
@@ -39,15 +44,43 @@ ll mod_pow(ll x, ll p, ll M) {
 	return a;
 }
 
-ll mod_inverse(ll a, ll m) {
-	return mod_pow(a, m - 2, m);
+ll mod_inverse(ll a, ll M = MOD) {
+	return mod_pow(a, M - 2, M);
 }
 
-ll nCr(ll n, ll r) {
+void set_fact(ll n, ll M = MOD) {
+	fact[0] = fact[1] = rfact[0] = rfact[1] = 1;
+	for (ll i = 2; i <= n; i++) {
+		fact[i] = i * fact[i - 1] % M;
+		rfact[i] = mod_inverse(fact[i], M);
+	}
+}
+
+ll nCr(ll n, ll r, ll M = MOD) {
 	ll ret = fact[n];
-	ret = (ret*mod_inverse(fact[r], MOD)) % MOD;
-	ret = (ret*mod_inverse(fact[n - r], MOD)) % MOD;
+	ret = (ret*rfact[r]) % M;
+	ret = (ret*rfact[n - r]) % M;
 	return ret;
+}
+
+// 第二種スターリング数
+// 互いに区別できるn個のものを区別のないk個のグループに分けるとき,その分け方はsecondStirling[n][k]
+// ただし空のグループがあってはいけない
+void setSecondStirling(ll n, ll M = MOD) {
+	secondStirling[0][0] = 1;
+	rep1(i, n) {
+		rep(j, i + 1) {
+			if (j == i) {
+				secondStirling[i][j] = 1;
+			}
+			else if (j == 0) {
+				secondStirling[i][j] = 0;
+			}
+			else {
+				secondStirling[i][j] = (secondStirling[i - 1][j - 1] + (ll)j*secondStirling[i - 1][j] % M) % M;
+			}
+		}
+	}
 }
 
 
