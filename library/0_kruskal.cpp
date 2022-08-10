374ec5831461c6918817ad33a418e7cb3537499c
fuppy0716 <n.fuppy@gmail.com>
2022-08-11 00:13:43 +0900

    initial

--- library/0_kruskal.cpp
+++ library/0_kruskal.cpp
@@ -77,126 +77,125 @@
 #include <system_error>
 #include <thread>
 #include <tuple>
-#include <typeindex>
 #include <type_traits>
+#include <typeindex>
 #include <unordered_map>
 #include <unordered_set>
 #endif
 using namespace std;
 
-
-#define DEBUG(x) cout<<#x<<": "<<x<<endl;
-#define DEBUG_VEC(v) cout<<#v<<":";for(int i=0;i<v.size();i++) cout<<" "<<v[i]; cout<<endl
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define DEBUG_VEC(v)                   \
+    cout << #v << ":";                 \
+    for (int i = 0; i < v.size(); i++) \
+        cout << " " << v[i];           \
+    cout << endl
 
 typedef long long ll;
 #define vi vector<int>
 #define vl vector<ll>
-#define vii vector< vector<int> >
-#define vll vector< vector<ll> >
+#define vii vector<vector<int>>
+#define vll vector<vector<ll>>
 #define vs vector<string>
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define pis pair<int,string>
-#define psi pair<string,int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pis pair<int, string>
+#define psi pair<string, int>
 const int inf = 1000000001;
 const ll INF = 1e16;
 #define MOD 1000000007
 #define mod 1000000009
 #define pi 3.14159265358979323846
-#define Sp(p) cout<<setprecision(15)<<fixed<<p<<endl;
-int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 };
-int dx2[8] = { 1,1,0,-1,-1,-1,0,1 }, dy2[8] = { 0,1,1,1,0,-1,-1,-1 };
-
-
-
-
+#define Sp(p) cout << setprecision(15) << fixed << p << endl;
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+int dx2[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy2[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 const int N = 1010;
 const int M = 2010;
 int n;
 
 class UnionFind {
-public:
-	int n;
-	vi par; //e
-	vi ran; //–Ø‚Ì[‚³
-	vi num; //—v‘f”
+  public:
+    int n;
+    vi par; //ï¿½e
+    vi ran; //ï¿½Ø‚Ì[ï¿½ï¿½
+    vi num; //ï¿½vï¿½fï¿½ï¿½
 
-	UnionFind(int _n) {
-		n = _n;
-		par.resize(n); ran.resize(n); num.resize(n);
-		for (int i = 0; i < n; i++) {
-			par[i] = i; ran[i] = 0; num[i] = 1;
-		}
-	}
+    UnionFind(int _n) {
+        n = _n;
+        par.resize(n);
+        ran.resize(n);
+        num.resize(n);
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            ran[i] = 0;
+            num[i] = 1;
+        }
+    }
 
-	//–Ø‚Ìª‚ð‹‚ß‚é
-	int find(int x) {
-		if (par[x] == x) {
-			return x;
-		}
-		else {
-			return par[x] = find(par[x]);
-		}
-	}
+    //ï¿½Ø‚Ìï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ß‚ï¿½
+    int find(int x) {
+        if (par[x] == x) {
+            return x;
+        } else {
+            return par[x] = find(par[x]);
+        }
+    }
 
-	//x‚Æy‚Ì‘®‚·‚éW‡‚ð•¹‡
-	void unite(int x, int y) {
-		x = find(x); y = find(y);
-		int numsum = num[x] + num[y];
-		if (x == y) {
-			return;
-		}
-		if (ran[x]<ran[y]) {
-			par[x] = y;
-		}
-		else {
-			par[y] = x;
-			if (ran[x] == ran[y]) {
-				ran[x]++;
-			}
-		}
-		num[x] = num[y] = numsum;
-	}
+    //xï¿½ï¿½yï¿½Ì‘ï¿½ï¿½ï¿½ï¿½ï¿½Wï¿½ï¿½ï¿½ð•¹ï¿½
+    void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        int numsum = num[x] + num[y];
+        if (x == y) {
+            return;
+        }
+        if (ran[x] < ran[y]) {
+            par[x] = y;
+        } else {
+            par[y] = x;
+            if (ran[x] == ran[y]) {
+                ran[x]++;
+            }
+        }
+        num[x] = num[y] = numsum;
+    }
 
-	//x‚Æy‚ª“¯‚¶W‡‚É‘®‚·‚é‚©”Û‚©
-	bool same(int x, int y) {
-		return find(x) == find(y);
-	}
-
+    //xï¿½ï¿½yï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Wï¿½ï¿½ï¿½É‘ï¿½ï¿½ï¿½ï¿½é‚©ï¿½Û‚ï¿½
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
 };
 
-vector< pair<ll, pll> > edges(M);
+vector<pair<ll, pll>> edges(M);
 
 ll kruskal(int n) {
-	sort(edges.begin(), edges.end());
-	UnionFind uf(n);
-	ll res = 0;
-	for (int i = 0; i < edges.size(); i++) {
-		if (!uf.same(edges[i].second.first, edges[i].second.second)) {
-			uf.unite(edges[i].second.first, edges[i].second.second);
-			res += edges[i].first;
-		}
-	}
-	return res;
+    sort(edges.begin(), edges.end());
+    UnionFind uf(n);
+    ll res = 0;
+    for (int i = 0; i < edges.size(); i++) {
+        if (!uf.same(edges[i].second.first, edges[i].second.second)) {
+            uf.unite(edges[i].second.first, edges[i].second.second);
+            res += edges[i].first;
+        }
+    }
+    return res;
 }
 
-
 int main() {
-	ll q;
-	cin >> q;
-	int i0;
-	for (i0 = 0; i0 < q; i0++) {
-		int i, j;
-		edges.clear();
-		cin >> n >> m >> r;
-		for (i = 0; i < r; i++) {
-			ll a, b, c;
-			cin >> a >> b >> c;
-			b += n;
-			edges.push_back(make_pair(-c, make_pair(a, b)));
-		}
-		cout << kruskal() + 10000 * (m + n) << endl;
-
-	}
+    ll q;
+    cin >> q;
+    int i0;
+    for (i0 = 0; i0 < q; i0++) {
+        int i, j;
+        edges.clear();
+        cin >> n >> m >> r;
+        for (i = 0; i < r; i++) {
+            ll a, b, c;
+            cin >> a >> b >> c;
+            b += n;
+            edges.push_back(make_pair(-c, make_pair(a, b)));
+        }
+        cout << kruskal() + 10000 * (m + n) << endl;
+    }
 }
